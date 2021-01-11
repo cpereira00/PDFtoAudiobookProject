b86a7dc820dbb2c77af964fd504683bb5bd53ffe
chris pereira <cpereira72500@gmail.com>
2021-01-11 15:38:31 -0500

    Added more functionality for users and exception handling

--- PDFtoAudioBook.py
+++ PDFtoAudioBook.py
@@ -1,34 +1,79 @@
 import PyPDF2
 import pyttsx3
 
-#replace "Proposal" with any PDF
+def words_per_minute():
+    WPM = int(input(f"How many words per minute(WPM) do you want the PDF to be read at (recommended 160-200): "))
+    speaker.setProperty('rate', WPM)
+
+def replay():
+
+    choice = input("Would you like to read more pages? Yes or No: ").lower()
+
+    return choice == 'yes'
+
+
+print('Welcome to your personal PDF to AudioBook Converter! \n')
+
+# replace "Proposal" with any PDF, ask user for there file instead
 engine = open('Proposal.pdf', 'rb')
 reader = PyPDF2.PdfFileReader(engine)
 page_count = reader.numPages
-print(f'number of pages is: {page_count-1}')
+print(f'The number of pages is: {page_count-1}')
 
 speaker = pyttsx3.init()
 
+while True:
+    print(f"If you want the entire PDF to be read, type 'entire'. Otherwise type 'other' for more options and 'leave' to terminate.")
+    pagesToRead = input('Your Choice: ').lower()
 
-print(f"Type 'entire' if you want the entire pdf to be read. Otherwise type the page number you want read")
-pagesToRead = input('Your Choice: ')
+    if pagesToRead == 'entire':
 
-WPM = input(f"How many words per minute(WPM) do you want the PDF to be read at (recommended 160-200): ")
-speaker.setProperty('rate', WPM)
+        WPM = int(input(f"How many words per minute(WPM) do you want the PDF to be read at (recommended 160-200): "))
+        speaker.setProperty('rate', WPM)
 
-if pagesToRead == 'entire':
+        for num in range(0, page_count):
+            page = reader.getPage(num)
+            words = page.extractText()
+            speaker.say(words)
+            speaker.runAndWait()
 
-    for num in range(0, page_count):
-        page = reader.getPage(num)
-        words = page.extractText()
-        speaker.say(words)
-        speaker.runAndWait()
 
-elif int(pagesToRead) >= page_count:
-    raise Exception("Sorry that page number is not within the book, program terminating!")
+    elif pagesToRead == 'other':
 
-else:
-    page = reader.getPage(int(pagesToRead))
-    words = page.extractText()
-    speaker.say(words)
-    speaker.runAndWait()
+        print('If you want only 1 page to be read, enter that page. '
+              '\nIf you want a range of pages to be read, enter the first page number followed by a space then the last page number.')
+
+        try:
+            inp = list(map(int,input('Your Choice: \n').split()))
+
+            if inp[0] < 0 or inp[1] > page_count:
+                print("Sorry, these pages don't exist as they are not within the book.")
+                break
+
+            words_per_minute()
+
+            for num in range(inp[0], inp[1]+1):
+                page = reader.getPage(num)
+                words = page.extractText()
+                speaker.say(words)
+                speaker.runAndWait()
+        except:
+
+            page = reader.getPage(inp[0])
+            words = page.extractText()
+            speaker.say(words)
+            speaker.runAndWait()
+
+    elif pagesToRead == 'leave':
+        break
+
+    else:
+        print('Oops, invalid input, try again. \n')
+        continue
+
+
+    if not replay():
+        break
+
+
+print('Program Terminating, have a nice day!')
